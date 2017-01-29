f10604989e4e55cfd52e3042cd1a5d38ce81e83a
TrifonEstov <don.tito.g@gmail.com>
2017-01-30 00:12:05 +0200

    PrimeChecker does not work properly. GeometryCalculator works 100/100 but not with different methods.

---
+++ MethodsExercises/CenterPoint/CenterPoint.cs
@@ -0,0 +1,20 @@
+﻿namespace CenterPoint
+{
+    using System;
+
+    public class CenterPoint
+    {
+        public static void Main()
+        {
+            var x1 = int.Parse(Console.ReadLine());
+            var y1 = int.Parse(Console.ReadLine());
+            var x2 = int.Parse(Console.ReadLine());
+            var y2 = int.Parse(Console.ReadLine());
+
+            if (x1 < x2 && y1 < y2)
+            {
+                Console.WriteLine($"({}");
+            }
+        }
+    }
+}
---
+++ MethodsExercises/GeometryCalculator/GeometryCalculator.cs
@@ -0,0 +1,40 @@
+﻿namespace GeometryCalculator
+{
+    using System;
+
+    public class GeometryCalculator
+    {
+        public static void Main()
+        {
+            string type = Console.ReadLine();
+            type = type.ToLower();
+
+            if (type == "triangle")
+            {
+                var triangleSide = double.Parse(Console.ReadLine());
+                var triangleHeight = double.Parse(Console.ReadLine());
+                var triangleArea = triangleSide * triangleHeight / 2;
+                Console.WriteLine(triangleArea.ToString("0.00"));
+            }
+            else if (type == "square")
+            {
+                var squareSide = double.Parse(Console.ReadLine());
+                var squareArea = Math.Pow(squareSide, 2);
+                Console.WriteLine(squareArea.ToString("0.00"));
+            }
+            else if (type == "rectangle")
+            {
+                var rectangleSide = double.Parse(Console.ReadLine());
+                var rectangleHeight = double.Parse(Console.ReadLine());
+                var rectangleArea = rectangleSide * rectangleHeight;
+                Console.WriteLine(rectangleArea.ToString("0.00"));
+            }
+            else if (type == "circle")
+            {
+                var circleRadius = double.Parse(Console.ReadLine());
+                var circleArea = Math.PI * Math.Pow(circleRadius, 2);
+                Console.WriteLine(circleArea.ToString("0.00"));
+            }
+        }
+    }
+}
