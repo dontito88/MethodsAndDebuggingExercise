74ac81e3d1ece58828429ddb873394fb81ccaec6
TrifonEstov <don.tito.g@gmail.com>
2017-01-29 15:53:50 +0200

    New program added!

---
+++ MethodsExercises/HelloName/HelloName.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloName
+{
+    using System;
+
+    public class HelloName
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine($"Hello, {PrintName(input)}");
+        }
+
+        private static string PrintName(string name)
+        {
+            return name;
+        }
+    }
+}
