2e17e5288893486959d1602a0f80cdcfeed33db4
TrifonEstov <don.tito.g@gmail.com>
2017-01-29 17:13:07 +0200

    Another problem solved!

---
+++ MethodsExercises/EnglishNameOfLastDigit/EnglishNameOfLastDigit.cs
@@ -0,0 +1,60 @@
+﻿namespace EnglishNameOfLastDigit
+{
+    using System;
+
+    public class EnglishNameOfLastDigit
+    {
+        public static void Main()
+        {
+            var input = long.Parse(Console.ReadLine());
+            var lastDigit = LastDigit(input);
+            var digitToWord = ConverDigitToWord(lastDigit);
+            Console.WriteLine(digitToWord);
+        }
+
+        private static long LastDigit(long digit)
+        {
+            long lastDigit = Math.Abs(digit % 10);
+            return lastDigit;
+        }
+
+        private static string ConverDigitToWord(long lastDigit)
+        {
+            var digitAsWord = string.Empty;
+            switch (lastDigit)
+            {
+                case 1:
+                    digitAsWord = "one";
+                    break;
+                case 2:
+                    digitAsWord = "two";
+                    break;
+                case 3:
+                    digitAsWord = "three";
+                    break;
+                case 4:
+                    digitAsWord = "four";
+                    break;
+                case 5:
+                    digitAsWord = "fice";
+                    break;
+                case 6:
+                    digitAsWord = "six";
+                    break;
+                case 7:
+                    digitAsWord = "seven";
+                    break;
+                case 8:
+                    digitAsWord = "eight";
+                    break;
+                case 9:
+                    digitAsWord = "nine";
+                    break;
+                case 0:
+                    digitAsWord = "zero";
+                    break;
+            }
+            return digitAsWord;
+        }
+    }
+}
