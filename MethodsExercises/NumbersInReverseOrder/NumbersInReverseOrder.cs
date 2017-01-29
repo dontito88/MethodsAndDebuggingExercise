cfc887571e3ef5dc9929e9aaa18daab581c51281
TrifonEstov <don.tito.g@gmail.com>
2017-01-29 18:00:01 +0200

    Another program added!

---
+++ MethodsExercises/NumbersInReverseOrder/NumbersInReverseOrder.cs
@@ -0,0 +1,27 @@
+﻿namespace NumbersInReverseOrder
+{
+    using System;
+
+    public class NumbersInReverseOrder
+    {
+        public static void Main()
+        {
+            var number = double.Parse(Console.ReadLine());
+            var numberInReverse = ReversedNumber(number);
+            Console.WriteLine(numberInReverse);
+        }
+
+        private static double ReversedNumber(double number)
+        {
+            var numString = number.ToString();
+            var reversedString = string.Empty;
+            for (int i = numString.Length - 1; i >= 0; i--)
+            {
+                reversedString += numString[i];
+            }
+
+            var reversedNum = double.Parse(reversedString);
+            return reversedNum;
+        }
+    }
+}
